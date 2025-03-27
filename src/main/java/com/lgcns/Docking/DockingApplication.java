1dfe243a979b7b078298f1d07dd08351613b8bcb
doheegu <doheegu3@gmail.com>
2025-03-27 13:37:46 +0900

    first commit

---
+++ src/main/java/com/lgcns/Docking/DockingApplication.java
@@ -0,0 +1,13 @@
+package com.lgcns.Docking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lgcns/Docking/DockingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgcns.Docking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
